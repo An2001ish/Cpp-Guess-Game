2858950fccb46392123c1c04d5708948dcab08b5
Anish Baraily <an2001ish@gmail.com>
2021-07-05 22:00:20 +0545

    Add project files.

---
+++ FISRST/Source.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+void savescore(int guess_size) {
+
+	ifstream input("score.txt");
+
+	int score;
+
+	input >> score;
+
+	ofstream output("score.txt");
+
+	if (guess_size < score) {
+
+		output << guess_size;
+	}
+	else {
+		output << score;
+	}
+
+}
+
+void print_vector(vector<int>guesses1) {
+
+	cout << "It took you " << guesses1.size()<< " tries to guess. Here are the guesses:" << endl; 
+	for (int i = 0; i <guesses1.size(); i++) {
+
+	cout << guesses1[i] << '\t';
+	}
+	cout << endl;
+}
+
+void guessgame() {
+	
+	cout << "Lets play a guessing game!\n";
+	int answer =rand()%101;
+	cout << answer << endl;
+	int count = 0;
+	vector <int> guesses;
+
+	while (true) {
+
+		int unum;
+		
+		cout << "Guess a number: ";
+
+		cin >> unum;
+
+		guesses.push_back(unum);
+
+		if (unum == answer) {
+			cout << "YOU WIN!\n";
+
+			break;
+		}
+		else if (unum < answer) {
+
+			cout << "TOO LOW!\n";
+		}
+		else {
+			cout << "TOO HIGH!\n";
+		}
+	}
+	
+	count = guesses.size();
+
+	savescore(count);
+
+print_vector(guesses);
+}
+
+int main() {
+
+	int choice;
+	srand(time(NULL));
+
+	do{
+			cout << "1. PLAY!\n0. QUIT\n";
+
+		cin >> choice;
+
+		switch(choice)  {
+
+		case 0:
+			return 0;
+
+		case 1:
+			
+			guessgame();
+		break;
+		}
+	
+	} while (choice != 0);
+
+	return 0;
+}
+
+
